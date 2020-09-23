d54472fadc07645b52f32ce34cf6e6de109511fb
shivonnu <shivonnu@gmail.com>
2020-09-23 18:00:46 +0900

    first commit
    indexから商品詳細ページに飛ぶようにリンクを貼りました。詳細ページに画像と名前と値段とコメントが表示されるようにshow.html.erbを編集しました。
    アソシエーションをする記述をItemモデルに追加、アイテムコントローラーにてアクティブハッシュの内容を変数に入れるように設定しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :days_to_ship, :shipping_area, :shipping_fee_burden, :status
+
   def index
     @items = Item.all.order("created_at DESC")
   end
   
   def new
     @item = Item.new
+  end
+
+  def show
+    @show = Item.find(params[:id]) 
+    @category = Category.all
+    @days_to_ship = Days_to_ship.all
+    @shipping_area = Shipping_area.all
+    @shipping_fee_burden = Shipping_fee_burden.all
+    @status = Status.all
   end
  
   def create
