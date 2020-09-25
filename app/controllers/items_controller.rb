c6054d66b8bf0014d147f1487602b79f4d2beb2c
shivonnu <shivonnu@gmail.com>
2020-09-25 21:57:58 +0900

    before_actionに重複メソッドをまとめる
    before_action で @item = Item.find_by(id: params[:id])をまとめました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).all.order("created_at DESC")
   end
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find_by(id: params[:id])
   end
  
   def create
@@ -25,11 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find_by(id: params[:id])
   end
 
   def update
-    @item = Item.find_by(id: params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,5 +40,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_burden_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
-  
+
+  def set_item
+    @item = Item.find_by(id: params[:id])
+  end
 end
