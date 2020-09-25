8ca24ed540d4f5703d176b24244774b489325ec4
shivonnu <shivonnu@gmail.com>
2020-09-25 22:19:04 +0900

    編集ページに出品者以外アクセスできないように修正
    editに出品者以外アクセスできないように修正、またログインしてないとアクセスできないようにbefore_actionにeditを追記しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).all.order("created_at DESC")
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if not current_user.id == @item.user_id
+       redirect_to root_path
+    end
   end
 
   def update
