0953dd8b9658a3754a360921a96c0ac0bc6485ed
shivonnu <shivonnu@gmail.com>
2020-09-27 23:05:24 +0900

    カード決済関連のフォームのid名を修正
    "name"を一回消す処理が入らなかったので、idを直したら通ったので修正しました。

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,6 +7,7 @@ const pay = ()=> {
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
     const card = {
+      
       number: formData.get("number"),
       cvc: formData.get("cvc"),
       exp_month: formData.get("exp_month"),
@@ -20,6 +21,11 @@ const pay = ()=> {
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+      
       document.getElementById("charge-form").submit();
       document.getElementById("charge-form").reset();
     })
