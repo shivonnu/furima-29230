3e48861dbac9f30e739aeb24e3e5e7c99584ed72
shivonnu <shivonnu@gmail.com>
2020-09-27 11:38:32 +0900

    購入テーブル都道府県カラム名を変更
    購入テーブルの都道府県カラム名をactivehashのshipping_area_idに統一。

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_061041) do
 
   create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefectures", null: false
+    t.integer "shipping_area_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building_name"
