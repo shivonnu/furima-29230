ea108542714998fb4dba796bbee8462e601aa877
shivonnu <shivonnu@gmail.com>
2020-09-27 12:13:21 +0900

    購入フォームのバリデーション設置
    購入フォーム建物名と外部キー以外のバリデーション設置

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,10 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :shipping_address
+  
+  validates  :postal_code,      presence: true
+  validates  :shipping_area_id, presence: true
+  validates  :city,             presence: true
+  validates  :address,          presence: true
+  validates  :phone_number,     presence: true
 end
