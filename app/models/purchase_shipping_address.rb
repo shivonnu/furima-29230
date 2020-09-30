6d76e6504a6119ff7d9b3a14acd30048c3bcc638
shivonnu <shivonnu@gmail.com>
2020-10-01 01:22:11 +0900

    tokenバリデーション、herokuの鍵環境設定
    herokuの鍵環境設定。herokuにプッシュはまだ。

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -3,6 +3,7 @@ class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor  :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token, :purchase_id
 
+  validates  :token,            presence: true
   validates  :postal_code,      presence: true
   validates  :shipping_area_id, presence: true
   validates  :city,             presence: true
