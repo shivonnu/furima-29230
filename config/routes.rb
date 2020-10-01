18c03e2dbc3a07e9d4daed1e7eb0334438827648
shivonnu <shivonnu@gmail.com>
2020-10-01 19:07:21 +0900

    購入テーブルにindex,createだけのルーティング追加
    処理を安定させる為に購入テーブルのルーティングをindex,createだけにしました。itemsは全種類実行したいのでそのままにしました。購入者情報は不要なルーティングだったので削除しました。コメントアウトも削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items do
-    resources :purchases
+    resources :purchases, only: [:index, :create]
   end
-  resources :shipping_addresses
   root to: "items#index"
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  end
