26c1d496aa8b73464693204c9440a0e88b91cfc0
shivonnu <shivonnu@gmail.com>
2020-10-01 15:38:57 +0900

    テストコード記入
    テストコード記入、実施確認

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -4,15 +4,18 @@ class PurchaseShippingAddress
   attr_accessor  :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token, :purchase_id
 
   validates  :token,            presence: true
-  validates  :postal_code,      presence: true
   validates  :shipping_area_id, presence: true
   validates  :city,             presence: true
   validates  :address,          presence: true
   validates  :phone_number,     presence: true
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)."}
+    validates :phone_number, format: {with: /\A[0-9]{9,11}\z/, message: "is invalid. Within 11 digits. Do not put hyphens."}
 
- def save
+  end
+ 
+  def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
- 
 end
---
+++ spec/factories/purchase_shipping_address.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchase_shipping_address do
+    
+    postal_code       { '123-4567' }
+    shipping_area_id  { '鈴木' }
+    city              { '東京都' }
+    address           { '府中市東雲町２丁目' }
+    building_name     { '東京ハイツ' }
+    phone_number      { '09012345678' }
+    token             { "tok_xxxxxxxx" }
+  end
+end
+
