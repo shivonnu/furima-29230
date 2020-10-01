1c2a591ba3214b710f5a3439f11a37d3d5ccd93d
shivonnu <shivonnu@gmail.com>
2020-10-01 16:55:14 +0900

    商品詳細ページ２箇所purchase.where修正し忘れ修正、バリデーションwith_optionsの中に３テーブル分まとめ
    商品詳細ページローカル環境のページでエラー確認して漏れに気づいたので修正、バリデーションwith_optionsの中に３テーブル分まとめました。

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -3,14 +3,10 @@ class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor  :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token, :purchase_id
 
-  validates  :token,            presence: true
-  validates  :shipping_area_id, presence: true
-  validates  :city,             presence: true
-  validates  :address,          presence: true
-  validates  :phone_number,     presence: true
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)."}
     validates :phone_number, format: {with: /\A[0-9]{9,11}\z/, message: "is invalid. Within 11 digits. Do not put hyphens."}
+    validates :token, :shipping_area_id, :city, :address, :phone_number
   end
  
   def save
