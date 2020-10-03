542be411de13a9de9af6100cc3b6906adfc94e3a
shivonnu <shivonnu@gmail.com>
2020-10-04 03:18:34 +0900

    first commit
    gem"aws-sdk-s3"インストール、
    aw3アカウント作成
    config書き換え
    ローカル環境で画像投稿確認。

--- spec/factories/purchase_shipping_address.rb
+++ spec/factories/purchase_shipping_address.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_shipping_address do
     postal_code       { '123-4567' }
-    shipping_area_id  { '鈴木' }
+    shipping_area_id  { '2' }
     city              { '東京都' }
     address           { '府中市東雲町２丁目' }
     building_name     { '東京ハイツ' }
