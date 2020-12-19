f64da971af33b302b8f60384147cce37b6b20239
shivonnu <shivonnu@gmail.com>
2020-12-20 00:47:50 +0900

    ログインしていないとコメントできないように編集
    できないこと確認済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,7 @@ class ItemsController < ApplicationController
     else
       flash.now
       render :new
+    
     end
   end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,17 @@ class MessagesController < ApplicationController
 
   def create
     @item = Item.find_by(id: params[:item_id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
     @message = MessageItemMessageAddress.new(message_params)
-    if #@message.valid?
+    if @message.valid?
        @message.save
       ActionCable.server.broadcast 'message_channel', content: @message
     else
       flash.now
       render :show
     end
+  end
   end
 
   private
