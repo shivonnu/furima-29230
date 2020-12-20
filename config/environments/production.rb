84b0977b51232da881bf74037dfba320548313ff
shivonnu <shivonnu@gmail.com>
2020-12-20 17:39:25 +0900

    Actioncable設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
    config.action_cable.url = 'ws://52.194.15.165/cable'
   
-   config.action_cable.allowed_request_origins = [ 'http://52.194.15.165', /http:\/\/52.194.15.165.*/ ]
+   config.action_cable.allowed_request_origins = [ '52.194.15.165' ]
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
