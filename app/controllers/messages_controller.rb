4730223185341425624ce6d0b94c15511c7353b8
shivonnu <shivonnu@gmail.com>
2020-12-21 22:56:44 +0900

    コメント設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,7 @@ class MessagesController < ApplicationController
     @message = MessageItemMessageAddress.new(message_params)
     if @message.valid?
        @message.save
+       redirect_to root_path
        #ActionCable.server.broadcast 'message_channel', content: "#{ @message.content }"
     else
       flash.now
