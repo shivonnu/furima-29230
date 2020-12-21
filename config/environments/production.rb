afda49a9d789e66a8f147f83d3ab2702fba6c0ab
shivonnu <shivonnu@gmail.com>
2020-12-21 15:07:40 +0900

    actioncable設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,12 +40,12 @@ Rails.application.configure do
    config.active_storage.service = :amazon
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+   config.action_cable.url = 'ws://52.194.15.165/cable'
   
    config.action_cable.allowed_request_origins = [ 'http://52.194.15.165/', /https:\/\/ドメイン.*/ ]
    config.action_cable.disable_request_forgery_protection = true
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-   config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
