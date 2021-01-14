c2bed00c6a969c5742410aea41796757f16a6f53
chiffon_cake <69893922+shivonnu@users.noreply.github.com>
2021-01-14 15:48:50 +0900

    コントローラー削除

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,4 @@
 class TagsController < ApplicationController
-  def search
-    @item = Item.find_by(id: params[:id])
-    @tag = ItemTag.includes(:tag).where(item_id: @item.id)
-    return nil if params[:keyword] == ""
-    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"] )
-    render json:{ keyword: tag }
-  end
+  
 
 end
