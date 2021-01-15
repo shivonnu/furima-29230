3d75b1e5d84e574a94c4c5cf9518e272f289161e
chiffon_cake <69893922+shivonnu@users.noreply.github.com>
2021-01-15 23:56:59 +0900

    unicorn設定変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,8 @@ pid "#{app_path}/tmp/pids/unicorn.pid"
 #pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+#listen 3000
 #listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
